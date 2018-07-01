f6fd7451529aa45bb7a4ef744679e0b7425d56af
Kiril Abaskin <kiril@1stdibs.com>
2018-07-01 16:49:36 +0300

    added multiple webpack entries

---
+++ src/components/Hello.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Hello extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello here!</h1>
+        <p className="about-me">
+          I am Kiril Abaskin, javascript developer located in Vilnius 🇱🇹, but
+          available for working around 🌎 🌏 🌍.<br /> <br />
+          I like to build modern web applications with <strong>node</strong>, <strong>react</strong>, <strong>relay</strong>{" "}
+          & <strong>graphql</strong> & <strong>☕️</strong>.
+        </p>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Hello;
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 module.exports = {
   // webpack folder's entry js - excluded from jekll's build process.
-  entry: "./webpack/entry.js",
+  entry: {
+    main: "./src/entries/mainpage.js",
+    about: "./src/entries/about.js"
+  },
   output: {
     // we're going to put the generated file in the assets folder so jekyll will grab it.
     path: __dirname + "/assets/javascripts/",
-    filename: "bundle.js"
+    filename: "[name].js"
   },
   module: {
     rules: [
