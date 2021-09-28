56e2da2228108692e365011cc98d7da304bf7b98
Kiril Abaskin <kiril.abashkin@gmail.com>
2021-09-28 22:27:56 +0300

    minor updates

--- main.js
+++ main.js
@@ -1,44 +1,46 @@
-const icon = {
-    light: "☀️",
-    dark: "🌒",
-};
+(() => {
+    const icon = {
+        light: "☀️",
+        dark: "🌒",
+    };
 
-const modeSwitcher = () => {
-    const currentMode = document.documentElement.getAttribute("data-theme");
-    const icon = currentMode === "dark" ? "🌒" : "☀️";
-    if (currentMode === "dark") {
-        document.documentElement.setAttribute("data-theme", "light");
-        window.localStorage.setItem("theme", "light");
-        document.getElementById("theme-toggle").innerHTML = icon;
+    const modeSwitcher = () => {
+        const currentMode = document.documentElement.getAttribute("data-theme");
+        const icon = currentMode === "dark" ? "🌒" : "☀️";
+        if (currentMode === "dark") {
+            document.documentElement.setAttribute("data-theme", "light");
+            window.localStorage.setItem("theme", "light");
+            document.getElementById("theme-toggle").innerHTML = icon;
+        } else {
+            document.documentElement.setAttribute("data-theme", "dark");
+            window.localStorage.setItem("theme", "dark");
+            document.getElementById("theme-toggle").innerHTML = icon;
+        }
+    };
+
+    const theme = localStorage.getItem("theme");
+    const userPrefersDark =
+        window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches;
+    const userPrefersLight =
+        window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: light)").matches;
+    const mediaTheme = userPrefersDark ? "dark" : "light";
+
+    if (theme) {
+        document.getElementById("theme-toggle").innerHTML =
+            theme === "light" ? icon["dark"] : icon["light"];
     } else {
-        document.documentElement.setAttribute("data-theme", "dark");
-        window.localStorage.setItem("theme", "dark");
-        document.getElementById("theme-toggle").innerHTML = icon;
+        document.getElementById("theme-toggle").innerHTML = userPrefersDark
+            ? icon["light"]
+            : icon["dark"];
     }
-};
 
-const theme = localStorage.getItem("theme");
-const userPrefersDark =
-    window.matchMedia &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches;
-const userPrefersLight =
-    window.matchMedia &&
-    window.matchMedia("(prefers-color-scheme: light)").matches;
-const mediaTheme = userPrefersDark ? "dark" : "light";
+    if (theme) {
+        document.documentElement.setAttribute("data-theme", theme);
+    } else {
+        document.documentElement.setAttribute("data-theme", mediaTheme);
+    }
 
-if (theme) {
-    document.getElementById("theme-toggle").innerHTML =
-        theme === "light" ? icon["dark"] : icon["light"];
-} else {
-    document.getElementById("theme-toggle").innerHTML = userPrefersDark
-        ? icon["light"]
-        : icon["dark"];
-}
-
-if (theme) {
-    document.documentElement.setAttribute("data-theme", theme);
-} else {
-    document.documentElement.setAttribute("data-theme", mediaTheme);
-}
-
-window.modeSwitcher = modeSwitcher;
+    window.modeSwitcher = modeSwitcher;
+})();
--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,14 @@ const { defineConfig } = require("vite");
 
 module.exports = defineConfig({
     build: {
+        watch: true,
         lib: {
             entry: path.resolve(__dirname, "main.js"),
             name: "main",
+            formats: ["es"],
             fileName: () => "main.js",
         },
         sourcemap: "inline",
+        outDir: "./assets/javascript/",
     },
 });
