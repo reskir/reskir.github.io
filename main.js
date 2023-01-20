c8a4655207162ece0d2a2f6b753cd3f60002ec6e
Kiril Abaskin <kiril.abashkin@gmail.com>
2023-01-20 17:19:01 +0200

    update vite

--- main.js
+++ main.js
@@ -1,47 +1,47 @@
 (() => {
     const icon = {
-        light: "☀️",
-        dark: "🌒",
+        light: '☀️',
+        dark: '🌒',
     };
 
     document.querySelector(
-        ".wrapper"
+        '.wrapper'
     ).innerHTML = `<button class="switcher" id="theme-toggle"></button>`;
 
-    const switcher = document.querySelector(".switcher");
+    const switcher = document.querySelector('.switcher');
 
-    switcher.addEventListener("click", () => {
-        const currentMode = document.documentElement.getAttribute("data-theme");
-        const icon = currentMode === "dark" ? "🌒" : "☀️";
-        if (currentMode === "dark") {
-            document.documentElement.setAttribute("data-theme", "light");
-            window.localStorage.setItem("theme", "light");
-            document.getElementById("theme-toggle").innerHTML = icon;
+    switcher.addEventListener('click', () => {
+        const currentMode = document.documentElement.getAttribute('data-theme');
+        const icon = currentMode === 'dark' ? '🌒' : '☀️';
+        if (currentMode === 'dark') {
+            document.documentElement.setAttribute('data-theme', 'light');
+            window.localStorage.setItem('theme', 'light');
+            document.getElementById('theme-toggle').innerHTML = icon;
         } else {
-            document.documentElement.setAttribute("data-theme", "dark");
-            window.localStorage.setItem("theme", "dark");
-            document.getElementById("theme-toggle").innerHTML = icon;
+            document.documentElement.setAttribute('data-theme', 'dark');
+            window.localStorage.setItem('theme', 'dark');
+            document.getElementById('theme-toggle').innerHTML = icon;
         }
     });
 
-    const theme = localStorage.getItem("theme");
+    const theme = localStorage.getItem('theme');
     const userPrefersDark =
         window.matchMedia &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches;
+        window.matchMedia('(prefers-color-scheme: dark)').matches;
     const userPrefersLight =
         window.matchMedia &&
-        window.matchMedia("(prefers-color-scheme: light)").matches;
-    const mediaTheme = userPrefersDark ? "dark" : "light";
+        window.matchMedia('(prefers-color-scheme: light)').matches;
+    const mediaTheme = userPrefersDark ? 'dark' : 'light';
 
     if (theme) {
-        switcher.innerHTML = theme === "light" ? icon["dark"] : icon["light"];
+        switcher.innerHTML = theme === 'light' ? icon['dark'] : icon['light'];
     } else {
-        switcher.innerHTML = userPrefersDark ? icon["light"] : icon["dark"];
+        switcher.innerHTML = userPrefersDark ? icon['light'] : icon['dark'];
     }
 
     if (theme) {
-        document.documentElement.setAttribute("data-theme", theme);
+        document.documentElement.setAttribute('data-theme', theme);
     } else {
-        document.documentElement.setAttribute("data-theme", mediaTheme);
+        document.documentElement.setAttribute('data-theme', mediaTheme);
     }
 })();
