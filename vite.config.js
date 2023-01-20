39c1f2073c9702ef57124e60af60f294917b45b5
Kiril Abaskin <kiril.abashkin@gmail.com>
2023-01-20 17:19:01 +0200

    update vite

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-const path = require("path");
-const { defineConfig } = require("vite");
+const path = require('path');
+const { defineConfig } = require('vite');
 
 module.exports = defineConfig({
     build: {
         watch: true,
         lib: {
-            entry: path.resolve(__dirname, "main.js"),
-            name: "main",
-            formats: ["es"],
-            fileName: () => "main.js",
+            entry: path.resolve(__dirname, 'main.js'),
+            name: 'main',
+            formats: ['es'],
+            fileName: () => 'main.js',
         },
-        sourcemap: "inline",
-        outDir: "./assets/javascript/",
+        sourcemap: 'inline',
+        outDir: './assets/javascript/',
     },
 });
