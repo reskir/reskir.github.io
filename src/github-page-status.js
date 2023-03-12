ebcb70957bf917629ce4bf96c9de1e92c27999e8
kirilabaskin <123543579+kirilabaskin@users.noreply.github.com>
2023-03-12 19:16:39 +0200

    add git info (#31)
    * add info about page build
    
    * use react-query
    
    * update yarn
    
    * fixup! use react-query
    
    * fixup! update yarn
    
    * fixup! use react-query
    
    * fixup! use react-query

---
+++ src/github-page-status.js
@@ -0,0 +1,92 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+    QueryClient,
+    QueryClientProvider,
+    useQuery,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: Infinity,
+        },
+    },
+});
+
+const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+};
+
+export const GHPageStatus = () => {
+    const { data, isLoading, isError } = useQuery({
+        queryKey: ["git-status"],
+        queryFn: async () => {
+            const response = await fetch(
+                "https://api.github.com/repos/reskir/reskir.github.io/pages/builds/latest",
+                {
+                    headers: {
+                        "Content-Type": "application/vnd.github+json",
+                    },
+                }
+            );
+            return await response.json();
+        },
+    });
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (isError) {
+        return null;
+    }
+
+    return (
+        <>
+            <h4>Latest build of this webpage</h4>
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    gap: "18px",
+                    alignItems: "center",
+                }}
+            >
+                <div>
+                    <a href={data.pusher.html_url}>
+                        <img
+                            style={{ borderRadius: "50%" }}
+                            src={data.pusher.avatar_url}
+                            width="50"
+                        />
+                    </a>
+                </div>
+                <div>
+                    <div>
+                        Status: {data.status} ✅ by{" "}
+                        <a href={data.pusher.html_url}>{data.pusher.login}</a>
+                    </div>
+                    <div>
+                        Created at:{" "}
+                        {new Date(data.created_at).toLocaleDateString(
+                            "en-US",
+                            options
+                        )}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+const root = ReactDOM.createRoot(document.querySelector("#github-status"));
+root.render(
+    <QueryClientProvider client={queryClient}>
+        <GHPageStatus />
+    </QueryClientProvider>
+);
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+    mode: "production",
+    externals: {
+        react: "React",
+        "react-dom": "ReactDOM",
+    },
+    watch: true,
+    watchOptions: {
+        ignored: /node_modules/,
+    },
+    entry: {
+        "github-status": path.resolve(
+            __dirname,
+            "src",
+            "github-page-status.js"
+        ),
+        main: path.resolve(__dirname, "main.js"),
+    },
+    output: {
+        path: path.resolve(__dirname, "assets/javascript"),
+        filename: "[name].js",
+    },
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false,
+                terserOptions: {
+                    format: {
+                        comments: false,
+                    },
+                },
+            }),
+        ],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(jsx|js)$/,
+                include: path.resolve(__dirname, "src"),
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: [
+                                [
+                                    "@babel/preset-env",
+                                    {
+                                        targets: "defaults",
+                                    },
+                                ],
+                                "@babel/preset-react",
+                            ],
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+};
